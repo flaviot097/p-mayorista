d50e5af5d5a2d7e912520cd554e611e06470ec82
flaviot097 <112975530+flaviot097@users.noreply.github.com>
2024-06-30 22:18:56 -0300

    adsadepeordmd

--- assets/js/reproducir-cartas-prod.js
+++ assets/js/reproducir-cartas-prod.js
@@ -26,63 +26,52 @@ function agregarProductoACarrito(imputvalue) {
   document.cookie = `carrito=${arreglo.join(",")};max-age=3600;`;
 }
 
-async function renderCards(productos) {
-  for (const jsonDatos of productos) {
-    let imagenBase64 = "../../uploads/1718691069537.jpg"; // Ruta a una imagen predeterminada
-
+function renderCards(productos) {
+  productos.forEach((jsonDatos) => {
+    var imagenBase64;
     if (jsonDatos.imagen && jsonDatos.imagen.data) {
-      // Convertir los datos de imagen a Base64
-      imagenBase64 = await bufferToBase64(jsonDatos.imagen.data);
+      imagenBase64 = bufferToBase64(jsonDatos.imagen.data);
     } else {
-      // Manejo si no hay imagen disponible
-      console.error(
-        `No hay datos de imagen para el producto ${jsonDatos.codigo}`
-      );
+      imagenBase64 = "../../uploads/1718690876436.png"; // Puedes reemplazar esto con una imagen predeterminada
     }
-
     const containerCards = `
-      <div loading="lazy" class="wow fadeInUp" id="${jsonDatos.codigo}">
-        <div class="card card-body border-0 text-center shadow pt-5 tarjeta-productos">
-          <form method="post" class="form-action-redirection" action="https://p-mayorista.onrender.com/producto.php" id="${jsonDatos.codigo}" value="${jsonDatos.codigo}">
-            <input class="input-disabled" type="text" name="code" value="${jsonDatos.codigo}">
-            <div class="svg-icon mx-auto mb-4">
-              <img loading="lazy" src="${imagenBase64}" alt="" class="img-productos" id="${jsonDatos.codigo}">
-            </div>
-            <h5 class="fg-gray nombre-producto" id="${jsonDatos.codigo}">${jsonDatos.producto}</h5>
-            <p id="${jsonDatos.codigo}" class="id-producto">Código de producto: ${jsonDatos.codigo}</p>
-            <p class="fs-small" id="${jsonDatos.codigo}">${jsonDatos.descripcion}</p>
-            <p id="${jsonDatos.codigo}" class="proveedor">Proveedor: ${jsonDatos.distribuidora}</p>
-            <h6 id="${jsonDatos.codigo}" class="precio-producto">Precio: $${jsonDatos.precio} c/u</h6>
-          </form>
-          <button id="${jsonDatos.codigo}" class="btn-agregar-carrito">
-            <img width="32" height="30" src="https://img.icons8.com/pastel-glyph/64/FFFFFF/shopping-trolley--v2.png" alt="shopping-trolley--v2" />
-          </button>
-        </div>
-      </div>`;
+            <div loading="lazy" class="wow fadeInUp" id="${jsonDatos.codigo}">
+                <div class="card card-body border-0 text-center shadow pt-5 tarjeta-productos">
+                    <form method="post" class="form-action-redirection" action="https://p-mayorista.onrender.com/producto.php" id="${jsonDatos.codigo}" value="${jsonDatos.codigo}">
+                        <input class="input-disabled" type="text" name="code">
+                        <div class="svg-icon mx-auto mb-4">
+                            <img loading="lazy" src="${imagenBase64}" alt="" class="img-productos" id="${jsonDatos.codigo}">
+                        </div>
+                        <h5 class="fg-gray nombre-producto" id="${jsonDatos.codigo}">${jsonDatos.producto}</h5>
+                        <p id="${jsonDatos.codigo}" class="id-producto">codigo de producto:${jsonDatos.codigo}</p>
+                        <p class="fs-small" id="${jsonDatos.codigo}">${jsonDatos.descripcion}.</p>
+                        <p id="${jsonDatos.codigo}" class="proveedor"> Proveedor: ${jsonDatos.distribuidora}.</p>
+                        <h6 id="${jsonDatos.codigo}" class="precio-producto">Precio: $${jsonDatos.precio} c/u</h6>
+                    </form>
+                    <button id=${jsonDatos.codigo} class="btn-agregar-carrito">
+                        <img width="32" height="30" src="https://img.icons8.com/pastel-glyph/64/FFFFFF/shopping-trolley--v2.png" alt="shopping-trolley--v2" />
+                    </button>
+                </div>
+            </div>`;
     const divCard = document.querySelector(".col-lg.contenedor-cartas");
     divCard.innerHTML += containerCards;
-  }
+  });
 
   // Añadir eventos después de crear las tarjetas
   addEventListeners();
 }
-
-async function bufferToBase64(buffer) {
-  return new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.onloadend = () => resolve(reader.result.split(",")[1]);
-    reader.onerror = reject;
-    reader.readAsDataURL(new Blob([buffer]));
-  });
+function bufferToBase64(buffer) {
+  // Convertir los datos de buffer a una cadena base64 válida
+  const base64String = Buffer.from(buffer).toString("base64");
+  return `data:image/jpeg;base64,${base64String}`;
 }
-
-async function reproducirCard() {
+function reproducirCard() {
   const nuevosProductos = dataFilter.slice(contador, contador + 8);
   renderCards(nuevosProductos);
   contador += 8; // Actualizar el contador
 }
 
-async function cardsonload() {
+function cardsonload() {
   const productosIniciales = dataFilter.slice(contador, contador + 8);
   renderCards(productosIniciales);
   contador += 8; // Actualizar el contador
